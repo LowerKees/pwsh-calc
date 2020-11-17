7ab826581c21d331e18976b528b9a268f6d5c008
Martijn Beenker <martijn.beenker@avanade.com>
2020-11-17 08:53:53 +0100

    Create test file skeleton

--- tests/calc.tests.ps1
+++ tests/calc.tests.ps1
@@ -1,1 +1,33 @@
 Import-Module Pester -MinimumVersion 5.0.0
+
+. $PSScriptRoot\..\calc\calc.ps1
+
+$TestCases = @{
+    Calc = [Calculator]::new();
+}
+
+Describe "The class Calculator and its properties" {
+    It "Should be instantiated without errors" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc | Should -BeOfType [Calculator]
+    }
+}
+Describe "The addition function of the calculator" {
+    It "Should add to integers" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc.Add(1,1) | Should -Be 2
+    }
+}
+
+# Unit tests for subtraction go here
+
+# Unit tests for multiplication go here
+
+# Unit tests for division go here
+
+# Unit test for squaring input go here
+
+# Unit test for cubing input go here
+
