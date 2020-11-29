ffe35265548156f0cde48044f6f10287a6888a32
Martijn Beenker <martijn.beenker@avanade.com>
2020-11-29 09:29:11 +0100

    Add solution for the unit tests

--- tests/calc.tests.ps1
+++ tests/calc.tests.ps1
@@ -14,20 +14,58 @@ Describe "The class Calculator and its properties" {
     }
 }
 Describe "The addition function of the calculator" {
-    It "Should add to integers" -TestCases $TestCases {
+    It "Should add two integers" -TestCases $TestCases {
         param($Calc)
 
         $Calc.Add(1,1) | Should -Be 2
     }
 }
 
-# Unit tests for subtraction go here
+Describe "The subtraction function of the calculator" {
+    It "Should subtract two integers" -TestCases $TestCases {
+        param($Calc)
 
-# Unit tests for multiplication go here
+        $Calc.Subtract(10, 5) | Should -Be 5
+    }
 
-# Unit tests for division go here
+    It "Should throw when non integer data types are used" -TestCases $TestCases {
+        param($Calc)
 
-# Unit test for squaring input go here
+        { $Calc.Subtract('string', 'string2') } | Should -Throw
+        { $Calc.Subtract(10, 'string2') } | Should -Throw
+        { $Calc.Subtract('string', 10) } | Should -Throw
+    }
+}
 
-# Unit test for cubing input go here
+Describe "The multiplication function of the calculator" {
+    It "Should multiply two integers" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc.Multiply(10, 5) | Should -Be 50
+    }
+}
+
+Describe "The division function of the calculator" {
+    It "Should divide two integers" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc.Divide(10, 5) | Should -Be 2
+    }
+}
+
+Describe "The squaring function of the calculator" {
+    It "Should return the second power of the input" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc.Power(10) | Should -Be 100
+    }
+}
+
+Describe "The cubing function of the calculator" {
+    It "Should return the third power of the input" -TestCases $TestCases {
+        param($Calc)
+
+        $Calc.Cube(10) | Should -Be 1000
+    }
+}
 
